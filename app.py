c9731715934a201d6b503e2aa55273f8f23bc2f2
AmirMahdyTurani <amir1386mahdy.t@gmail.com>
2021-09-29 19:11:23 +0330

    version 1 [does not have best gui]

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask
+import random
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html')
+
+
+@app.route('/result/')
+def result():
+    ud = random.randint(1, 2)
+    return render_template('result.html', ud=ud)
